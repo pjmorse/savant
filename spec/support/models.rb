9b62e2007c5cb0c738ba19ab64bc5230b1a5a586
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2013-10-24 13:10:19 -0400

    Remove attr_accessible calls in test schema.

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,16 +1,13 @@
 class Artist < ActiveRecord::Base
-  attr_accessible :name, :deceased
   has_many :albums
   has_many :songs, through: :albums
 end
 
 class Album < ActiveRecord::Base
-  attr_accessible :artist_id, :name, :released_on
   belongs_to :artist
   has_many :songs
 end
 
 class Song < ActiveRecord::Base
-  attr_accessible :album_id, :duration_in_seconds, :name
   belongs_to :album
 end
